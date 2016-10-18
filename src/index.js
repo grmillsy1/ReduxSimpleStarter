84f64a4aa37ba4efc4d37a3b68f4dd078fc51fb7
Georgia Mills <georgee_123@hotmail.com>
2016-10-18 11:49:54 +0100

    added Youtube Key

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 //create new component. This should produce some html
 
 //Take component's HTML and put it on page (in the DOM)
-
+require('dotvenv').config();
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+const API_KEY = API_KEY_YOUTUBE;
 
 const App = function() {
   return <div> HI! </div>
